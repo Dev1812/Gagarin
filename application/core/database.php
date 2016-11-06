29bcf1cfed6cce82e75e3e06856ecdb5e72bc750
queue_18 <timka_issaev@mail.ru>
2016-11-06 10:50:37 +0300

    now a new database name

--- application/core/database.php
+++ application/core/database.php
@@ -1,6 +1,6 @@
 <?php
 class DataBase{
-  public static function connect($host = 'localhost', $user = 'root', $password = '', $db_name = 'sgtu') {
+  public static function connect($host = 'localhost', $user = 'root', $password = '', $db_name = 'gagarin') {
     return new PDO('mysql:host='.$host.';dbname='.$db_name, $user, $password);
   }
 }
